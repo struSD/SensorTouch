113a30cac848457c998621de1038ffabe2cdf7dc
Danylo Strus <97678164+struSD@users.noreply.github.com>
2023-02-25 21:57:35 +0200

    Created form with control buttons, showing the touch points in dots (file represents by matrix output file).

--- SensorTouch/Form1.Designer.cs
+++ SensorTouch/Form1.Designer.cs
@@ -28,61 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.button1_OpenFile = new System.Windows.Forms.Button();
+            this.button2_Start = new System.Windows.Forms.Button();
+            this.button3_Stop = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2_XY = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4_Clear = new System.Windows.Forms.Button();
+            this.button5_AplyMaxValue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // button1_OpenFile
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(694, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open File";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1_OpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1_OpenFile.Location = new System.Drawing.Point(591, 12);
+            this.button1_OpenFile.Name = "button1_OpenFile";
+            this.button1_OpenFile.Size = new System.Drawing.Size(197, 29);
+            this.button1_OpenFile.TabIndex = 0;
+            this.button1_OpenFile.Text = "Open File";
+            this.button1_OpenFile.UseVisualStyleBackColor = true;
+            this.button1_OpenFile.Click += new System.EventHandler(this.button1_Click_OpenFile);
             // 
-            // button2
+            // button2_Start
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(694, 47);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Start";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2_Start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2_Start.Enabled = false;
+            this.button2_Start.Location = new System.Drawing.Point(591, 47);
+            this.button2_Start.Name = "button2_Start";
+            this.button2_Start.Size = new System.Drawing.Size(197, 29);
+            this.button2_Start.TabIndex = 1;
+            this.button2_Start.Text = "Start";
+            this.button2_Start.UseVisualStyleBackColor = true;
+            this.button2_Start.Click += new System.EventHandler(this.button2_Click_Start);
             // 
-            // button3
+            // button3_Stop
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(694, 82);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Stop";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3_Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3_Stop.Enabled = false;
+            this.button3_Stop.Location = new System.Drawing.Point(591, 82);
+            this.button3_Stop.Name = "button3_Stop";
+            this.button3_Stop.Size = new System.Drawing.Size(197, 29);
+            this.button3_Stop.TabIndex = 2;
+            this.button3_Stop.Text = "Stop";
+            this.button3_Stop.UseVisualStyleBackColor = true;
+            this.button3_Stop.Click += new System.EventHandler(this.button3_Click_Stop);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(591, 152);
+            this.trackBar1.Maximum = 2500;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(197, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Tag = "";
+            this.trackBar1.TickFrequency = 200;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(755, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "50ms";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(573, 406);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2_XY
+            // 
+            this.label2_XY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2_XY.AutoSize = true;
+            this.label2_XY.Location = new System.Drawing.Point(12, 421);
+            this.label2_XY.Name = "label2_XY";
+            this.label2_XY.Size = new System.Drawing.Size(62, 20);
+            this.label2_XY.TabIndex = 7;
+            this.label2_XY.Text = "X:0 - Y:0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(591, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 27);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "1000";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button4_Clear
+            // 
+            this.button4_Clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4_Clear.Enabled = false;
+            this.button4_Clear.Location = new System.Drawing.Point(591, 117);
+            this.button4_Clear.Name = "button4_Clear";
+            this.button4_Clear.Size = new System.Drawing.Size(197, 29);
+            this.button4_Clear.TabIndex = 12;
+            this.button4_Clear.Text = "Clear";
+            this.button4_Clear.UseVisualStyleBackColor = true;
+            this.button4_Clear.Click += new System.EventHandler(this.button4_Click_Clear);
+            // 
+            // button5_AplyMaxValue
+            // 
+            this.button5_AplyMaxValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5_AplyMaxValue.Enabled = false;
+            this.button5_AplyMaxValue.Location = new System.Drawing.Point(591, 247);
+            this.button5_AplyMaxValue.Name = "button5_AplyMaxValue";
+            this.button5_AplyMaxValue.Size = new System.Drawing.Size(197, 29);
+            this.button5_AplyMaxValue.TabIndex = 13;
+            this.button5_AplyMaxValue.Text = "Aply max value";
+            this.button5_AplyMaxValue.UseVisualStyleBackColor = true;
+            this.button5_AplyMaxValue.Click += new System.EventHandler(this.button5_Click_AplyMaxValue);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5_AplyMaxValue);
+            this.Controls.Add(this.button4_Clear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2_XY);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3_Stop);
+            this.Controls.Add(this.button2_Start);
+            this.Controls.Add(this.button1_OpenFile);
             this.Name = "Form1";
             this.Text = "SensorTouch";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
+        private Button button1_OpenFile;
+        private Button button2_Start;
+        private Button button3_Stop;
+        private TrackBar trackBar1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private Label label2_XY;
+        private TextBox textBox1;
+        private Button button4_Clear;
+        private Button button5_AplyMaxValue;
     }
 }
--- SensorTouch/Form1.cs
+++ SensorTouch/Form1.cs
@@ -1,18 +1,143 @@
+using System;
+
 namespace SensorTouch
 {
     public partial class Form1 : Form
     {
+        private int[,] pointArray = {
+            { 10, 10 },
+            { 30, 30 },
+            { 60, 60 },
+            { 190, 11 },
+            { 166, 300 },
+            { 6, 222 },
+            { 199, 147 },
+            {12, 88 }
+        };
+        int currentPointIndex = 0;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button3_Click(object sender, EventArgs e)
+        private void trackBar1_Scroll(object sender, EventArgs e)
         {
+            label1.Text = $"{String.Format(trackBar1.Value.ToString())}ms";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2_XY.Text = String.Format($"X:{pointArray[currentPointIndex, 1]} - Y:{pointArray[currentPointIndex, 0]}");
 
+            Graphics paintDot = pictureBox1.CreateGraphics();
+            paintDot.FillEllipse(Brushes.Red, pointArray[currentPointIndex, 1], pointArray[currentPointIndex, 0], 8, 8);
+
+            currentPointIndex++;
+
+            if (currentPointIndex >= pointArray.GetLength(0))
+            {
+                timer1.Stop();
+                currentPointIndex = 0;
+                trackBar1.Enabled = true;
+                trackBar1.Enabled = true;
+                button1_OpenFile.Enabled = true;
+                button4_Clear.Enabled = true;
+                button5_AplyMaxValue.Enabled = true;
+                textBox1.Enabled = true;
+            }
+        }
+        private void GetMaxNumbers(int[,] mass)
+       {
+            for (int x = 0; x < mass.GetLength(0); x++)
+            {
+                int max = mass[x, 0];
+                int y = 0;
+
+                for (int j = 1; j < mass.GetLength(1); j++)
+                {
+                    if (mass[x, j] > max)
+                    {
+                        max = mass[x, j];
+                        y = j;
+                    }
+                }
+                Console.WriteLine("Max value  X:{0},Y:{1}\t num:{2}", x, y, max);
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click_OpenFile(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+                    string[] lines = File.ReadAllLines(filePath);
+                    string[] size = lines[0].Split(',');
+                    int rowCount = lines.Length;
+                    int matrixNum1 = int.Parse(size[0]);
+                    int matrixNum2 = int.Parse(size[1]);
+                    int colCount = matrixNum1 * matrixNum2;
+
+                    int[,] data = new int[rowCount - 1, colCount];
+
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        string[] values = lines[i].Split(',');
+                        for (int j = 0; j < colCount; j++)
+                        {
+                            data[i - 1, j] = int.Parse(values[j]);
+                        }
+                    }
+
+                    trackBar1.Enabled = true;
+                    trackBar1.Enabled = true;
+                    button1_OpenFile.Enabled = true;
+                    button2_Start.Enabled = true;
+                    button3_Stop.Enabled = true;
+                    button4_Clear.Enabled = true;
+                    button5_AplyMaxValue.Enabled = true;
+                    textBox1.Enabled = true;
+                }
+            }
+        }
+        private void button2_Click_Start(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+            trackBar1.Enabled = false;
+            button1_OpenFile.Enabled = false;
+            button4_Clear.Enabled = false;
+            button5_AplyMaxValue.Enabled = false;
+            textBox1.Enabled = false;
+            timer1.Start();
+        }
+
+        private void button3_Click_Stop(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            trackBar1.Enabled = true;
+            trackBar1.Enabled = true;
+            button1_OpenFile.Enabled = true;
+            button4_Clear.Enabled = true;
+            button5_AplyMaxValue.Enabled = true;
+            textBox1.Enabled = true;
+        }
+
+        private void button4_Click_Clear(object sender, EventArgs e)
+        {
+            currentPointIndex = 0;
+            pictureBox1.Image = null;
+            label2_XY.Text = $"X:{0} - Y:{0}";
+        }
+
+        private void button5_Click_AplyMaxValue(object sender, EventArgs e)
         {
 
         }
